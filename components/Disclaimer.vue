985399c1c5a78cdd7e1ed4dc683189f80b382409
Nathakit Sae-Tan <nathakit.tan@gmail.com>
2021-06-27 18:57:45 +0700

    add disclaimer component

---
+++ components/Disclaimer.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+div.bg-gray-200
+  div.container.mx-auto.h-full
+    div.container-padding
+      div(style="max-width:560px;")
+        h3.pb-2 Disclaimer
+        p.pb-3.font-medium
+          | No affiliation with any government entity
+        p
+          | Attempts are made to retrieve data from aforementioned sources daily but is not guaranteed to be up to date.
+</template>
