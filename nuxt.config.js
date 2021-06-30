ff2b3197a51c1433ef4788353cffef8a887576e9
Nathakit Sae-Tan <nathakit.tan@gmail.com>
2021-07-01 01:12:30 +0700

    update head title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "src",
+    title: "Thailand Covid-19 Vaccination Tracker",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
