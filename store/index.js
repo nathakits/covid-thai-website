38f88fff8726e1c0124888ac7b6f36aeed84d300
Nathakit Sae-Tan <nathakit.tan@gmail.com>
2021-07-28 19:22:21 +0700

    add menu state to store

--- store/index.js
+++ store/index.js
@@ -1,15 +1,22 @@
 export const state = () => ({
   selected: "Total",
+  menuOpened: false,
 })
 
 export const mutations = {
   updateSelection: (state, value) => {
     state.selected = value
   },
+  menuState: (state, value) => {
+    state.menuOpened = value
+  },
 }
 
 export const getters = {
   selected: (state) => {
     return state.selected
+  },
+  menuOpened: () => {
+    return state.menuOpened
   },
 }
