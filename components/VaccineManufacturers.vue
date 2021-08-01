355a5eb64632a66c0d5808f5f1e73d907b9ef8f7
Nathakit Sae-Tan <nathakit.tan@gmail.com>
2021-08-01 18:38:37 +0700

    update copytext and font weight

--- components/VaccineManufacturers.vue
+++ components/VaccineManufacturers.vue
@@ -6,25 +6,35 @@ div.vaccination-manufacturers
     div.explainer.pb-4(
       class="lg:pb-0"
     )
-      h2.pb-2 Vaccinations Manufacturers
-      p 
-        | {{ manufacturers.filter(vac => vac.status === 'Approved').length }} vaccines approved for Use in Thailand.
-      p {{ manufacturers.filter(vac => vac.doses_administered > 0).length }} vaccines currently in use.
+      h2.pb-2 Vaccination Manufacturers
+      p
+        strong {{ manufacturers.filter(vac => vac.status === 'Approved').length }}
+        |  vaccines approved by the FDA (Food and Drug Administration) for use in Thailand.
+      p
+        strong {{ manufacturers.filter(vac => vac.doses_administered > 0).length }}
+        |  vaccines currently being administered to the public.
     div.progress-bar
-      div.card.grid.divide-y.divide-gray-300
-        div.card-item-padding.flex.justify-between
-          div.text-base.font-bold Vaccines
-          span.text-base.font-bold Total doses administered
-        div.card-item-padding.grid.grid-cols-2.gap-8(
-          v-for="vac in manufacturers"
-          :class="`${vac.name.split(' ').join('-').toLowerCase()} ${vac.status !== 'Approved' ? 'text-gray-400' : ''}`"
-        )
-          div
-            div.text-sm.font-bold {{ vac.name }}
+      //- div.relative
+      //-   div.responsive.bg-gray-100.rounded
+      //-   vaccine-linechart(:data="data")
+      div
+        div.card.grid.divide-y.divide-gray-300
+          div.card-item-padding.flex.justify-between
+            div.text-base.font-bold Vaccines
+            span.text-base.font-bold Doses Administered
+          div.card-item-padding.grid.grid-cols-2.gap-8(
+            v-for="vac in manufacturers"
+            :class="`${vac.name.split(' ').join('-').toLowerCase()} ${vac.status !== 'Approved' ? 'text-gray-400' : ''}`"
+          )
             div
-              span.text-sm.text-gray-500.vac-type {{ vac.type }}
-              span.text-sm.text-gray-500.vac-status {{ vac.status }}
-          span.text-base.flex.justify-end.items-center(:class="`${vac.status === 'Approved' ? 'text-black' : ''}`") {{ vac.doses_administered.toLocaleString() }}
+              div.text-base.font-medium {{ vac.name }}
+              div
+                span.text-sm.text-gray-500.vac-type {{ vac.type }}
+                span.text-sm.text-gray-500.vac-status {{ vac.status }}
+            span.text-base.flex.justify-end.items-center.font-medium(:class="`${vac.status === 'Approved' ? 'text-gray-800' :''}`") {{ vac.doses_administered.toLocaleString() }}
+          div.card-item-padding.grid.grid-cols-2.gap-8
+            div.text-base.font-bold Total
+            span.text-base.flex.justify-end.items-center.font-bold.text-gray-800 {{ totalVaccinesAdministered }}
 </template>
 
 <script>
@@ -42,10 +52,15 @@ export default {
         {
           name: "AstraZeneca",
           status: "Approved",
-          type: "Adenoviral",
+          type: "Viral Vector",
         },
         {
           name: "Moderna",
+          status: "Approved",
+          type: "mRNA",
+        },
+        {
+          name: "Pfizer",
           status: "Approved",
           type: "mRNA",
         },
@@ -127,6 +142,22 @@ export default {
         return vaccineArr
       } else {
         return []
+      }
+    },
+    totalVaccinesAdministered() {
+      if (!this.$fetchState.pending) {
+        const array = this.data
+        const latestArray = array[array.length - 1]
+        const astrazenecaDoses = latestArray.AstraZeneca
+        const sinovacDoses = latestArray.Sinovac
+        const sinopharmDoses = latestArray.Sinopharm
+        return (
+          astrazenecaDoses +
+          sinovacDoses +
+          sinopharmDoses
+        ).toLocaleString()
+      } else {
+        return 0
       }
     },
     getLastUpdated() {
