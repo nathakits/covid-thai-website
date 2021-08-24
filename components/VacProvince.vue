eec18f6777739d3a0d3be7ae64cf927de0e0677d
Nathakit Sae-Tan <nathakit.tan@gmail.com>
2021-08-25 01:55:57 +0700

    add show more/less button update table update province object updating total to % of population

--- components/VacProvince.vue
+++ components/VacProvince.vue
@@ -2,28 +2,29 @@
 div.vaccine-province
   div.date-padding.dark-blue.pt-6
     div.last-updated {{ `Last updated: ${data.update_date}` }}
-  div.vaccination-block.container-padding
-    div.explainer.pb-4(
-      class="lg:pb-0"
-    )
-      h2.pb-2 Province
-      p
-        | This table is sorted by provinces with the most vaccination.
-    div.progress-bar
   div.container-padding.table-container.w-full
-    table.w-full
-      thead.border-b.border-gray-300
-        tr
-          th.text-sm(v-for="head in header")
-            | {{ head }}
-      tbody
-        tr(v-for="p in provinces")
-          td.text-xs(class="sm:text-sm font-bold") {{ p.name }}
-          td.text-xs(class="sm:text-sm px-6")
-          td.text-xs(class="sm:text-sm") {{ p.total_1dose }}
-          td.text-xs(class="sm:text-sm") {{ p.total_2dose }}
-          td.text-xs(class="sm:text-sm") {{ p.total_3dose }}
-          td.text-xs(class="sm:text-sm") {{ p.totalDose.toLocaleString() }}
+    h2.pb-2 Province
+    p
+      | This table is sorted by provinces with the most vaccination.
+    div.py-4
+      template(v-if="provinces.length === 6")
+        button.button(@click="showMore()") Show all
+      template(v-else)
+        button.button(@click="showLess()") Show less
+    div.border.rounded-lg.shadow-lg
+      table.w-full
+        thead.border-b.border-gray-300
+          tr
+            th.text-sm(v-for="head in header")
+              | {{ head }}
+        tbody
+          tr(v-for="p in provinces")
+            td.text-xs.font-bold.w-20(class="sm:text-sm md:w-40 lg:w-52 xl:w-72") {{ p.name }}
+            //- td.text-xs(class="sm:text-sm px-6")
+            td.text-xs(class="sm:text-sm") {{ p.total_1dose }}
+            td.text-xs(class="sm:text-sm") {{ p.total_2dose }}
+            td.text-xs(class="sm:text-sm") {{ p.total_3dose }}
+            td.text-xs(class="sm:text-sm") 0%
 </template>
 
 <script>
@@ -31,85 +32,92 @@ export default {
   data() {
     return {
       data: {},
-      header: ["", "", "1st Dose", "2nd Dose", "3rd Dose", "Total"],
+      numProvinces: 6,
+      header: [
+        "",
+        "1st Dose",
+        "2nd Dose",
+        "3rd Dose",
+        "% of Population (2 Doses)",
+      ],
       province_th_map: {
-        Krabi: "กระบี่",
         Bangkok: "กรุงเทพมหานคร",
-        Kanchanaburi: "กาญจนบุรี",
-        Kalasin: "กาฬสินธุ์",
-        Kamphaengphet: "กำแพงเพชร",
-        Khonkaen: "ขอนแก่น",
-        Chanthaburi: "จันทบุรี",
-        Chachoengsao: "ฉะเชิงเทรา",
-        Chonburi: "ชลบุรี",
-        Chainat: "ชัยนาท",
-        Chaiyaphum: "ชัยภูมิ",
-        Chumphon: "ชุมพร",
-        Chiangrai: "เชียงราย",
-        Chiangmai: "เชียงใหม่",
-        Trang: "ตรัง",
-        Trat: "ตราด",
-        Tak: "ตาก",
-        Nakhonnayok: "นครนายก",
-        Nakhonpathom: "นครปฐม",
-        Nakhonphanom: "นครพนม",
-        Nakhonratchasima: "นครราชสีมา",
-        Nakhonsithammarat: "นครศรีธรรมราช",
-        Nakhonsawan: "นครสวรรค์",
-        Nonthaburi: "นนทบุรี",
-        Narathiwat: "นราธิวาส",
-        Nan: "น่าน",
+        Amnat_Charoen: "อำนาจเจริญ",
+        Ang_Thong: "อ่างทอง",
+        Bueng_Kan: "บึงกาฬ",
         Buriram: "บุรีรัมย์",
-        Pathumthani: "ปทุมธานี",
-        Prachuapkhirikhan: "ประจวบคีรีขันธ์",
-        Prachinburi: "ปราจีนบุรี",
-        Pattani: "ปัตตานี",
-        Ayutthaya: "พระนครศรีอยุธยา",
-        Phayao: "พะเยา",
-        Phangnga: "พังงา",
-        Phatthalung: "พัทลุง",
-        Phichit: "พิจิตร",
-        Phitsanulok: "พิษณุโลก",
-        Phetchaburi: "เพชรบุรี",
-        Phetchabun: "เพชรบูรณ์",
-        Phrae: "แพร่",
-        Phuket: "ภูเก็ต",
-        Mahasarakham: "มหาสารคาม",
-        Mukdahan: "มุกดาหาร",
-        Maehongson: "แม่ฮ่องสอน",
-        Yasothon: "ยโสธร",
-        Yala: "ยะลา",
-        Roiet: "ร้อยเอ็ด",
-        Ranong: "ระนอง",
-        Rayong: "ระยอง",
-        Ratchaburi: "ราชบุรี",
-        Lopburi: "ลพบุรี",
-        Loei: "เลย",
+        Chachoengsao: "ฉะเชิงเทรา",
+        Chai_Nat: "ชัยนาท",
+        Chaiyaphum: "ชัยภูมิ",
+        Chanthaburi: "จันทบุรี",
+        Chiang_Mai: "เชียงใหม่",
+        Chiang_Rai: "เชียงราย",
+        Chonburi: "ชลบุรี",
+        Chumphon: "ชุมพร",
+        Kalasin: "กาฬสินธุ์",
+        Kamphaeng_Phet: "กำแพงเพชร",
+        Kanchanaburi: "กาญจนบุรี",
+        Khon_Kaen: "ขอนแก่น",
+        Krabi: "กระบี่",
         Lampang: "ลำปาง",
         Lamphun: "ลำพูน",
-        Sisaket: "ศรีสะเกษ",
-        Sakonnakhon: "สกลนคร",
-        Songkhla: "สงขลา",
-        Satun: "สตูล",
-        Samutprakan: "สมุทรปราการ",
-        Samutsongkhram: "สมุทรสงคราม",
-        Samutsakhon: "สมุทรสาคร",
-        Sakaeo: "สระแก้ว",
+        Loei: "เลย",
+        Lopburi: "ลพบุรี",
+        Mae_Hong_Son: "แม่ฮ่องสอน",
+        Maha_Sarakham: "มหาสารคาม",
+        Mukdahan: "มุกดาหาร",
+        Nakhon_Nayok: "นครนายก",
+        Nakhon_Pathom: "นครปฐม",
+        Nakhon_Phanom: "นครพนม",
+        Nakhon_Ratchasima: "นครราชสีมา",
+        Nakhon_Sawan: "นครสวรรค์",
+        Nakhon_Si_Thammarat: "นครศรีธรรมราช",
+        Nan: "น่าน",
+        Narathiwat: "นราธิวาส",
+        Nong_Bua_Lamphu: "หนองบัวลำภู",
+        Nong_Khai: "หนองคาย",
+        Nonthaburi: "นนทบุรี",
+        Pathum_Thani: "ปทุมธานี",
+        Pattani: "ปัตตานี",
+        Phang_Nga: "พังงา",
+        Phatthalung: "พัทลุง",
+        Phayao: "พะเยา",
+        Phetchabun: "เพชรบูรณ์",
+        Phetchaburi: "เพชรบุรี",
+        Phichit: "พิจิตร",
+        Phitsanulok: "พิษณุโลก",
+        Phra_Nakhon_Si_Ayutthaya: "พระนครศรีอยุธยา",
+        Phrae: "แพร่",
+        Phuket: "ภูเก็ต",
+        Prachinburi: "ปราจีนบุรี",
+        Prachuap_Khiri_Khan: "ประจวบคีรีขันธ์",
+        Ranong: "ระนอง",
+        Ratchaburi: "ราชบุรี",
+        Rayong: "ระยอง",
+        Roi_Et: "ร้อยเอ็ด",
+        Sa_Kaeo: "สระแก้ว",
+        Sakon_Nakhon: "สกลนคร",
+        Samut_Prakan: "สมุทรปราการ",
+        Samut_Sakhon: "สมุทรสาคร",
+        Samut_Songkhram: "สมุทรสงคราม",
         Saraburi: "สระบุรี",
-        Singburi: "สิงห์บุรี",
+        Satun: "สตูล",
+        Sing_Buri: "สิงห์บุรี",
+        Sisaket: "ศรีสะเกษ",
+        Songkhla: "สงขลา",
         Sukhothai: "สุโขทัย",
-        Suphanburi: "สุพรรณบุรี",
-        Suratthani: "สุราษฎร์ธานี",
+        Suphan_Buri: "สุพรรณบุรี",
+        Surat_Thani: "สุราษฎร์ธานี",
         Surin: "สุรินทร์",
-        Nongkhai: "หนองคาย",
-        Nongbualamphu: "หนองบัวลำภู",
-        Angthong: "อ่างทอง",
-        Amnatcharoen: "อำนาจเจริญ",
-        Udonthani: "อุดรธานี",
+        Tak: "ตาก",
+        Trang: "ตรัง",
+        Trat: "ตราด",
+        Ubon_Ratchathani: "อุบลราชธานี",
+        Udon_Thani: "อุดรธานี",
+        Uthai_Thani: "อุทัยธานี",
         Uttaradit: "อุตรดิตถ์",
-        Uthaithani: "อุทัยธานี",
-        Ubonratchathani: "อุบลราชธานี",
-        Betong: "เบตง",
+        Yala: "ยะลา",
+        Yasothon: "ยโสธร",
       },
     }
   },
@@ -132,7 +140,7 @@ export default {
           }
           // map to new object
           const obj = {
-            name: provinceName,
+            name: provinceName.replace(/_/g, " "),
             population: 0,
             total_1dose: d.total_1st_dose.toLocaleString(),
             total_2dose: d.total_2nd_dose.toLocaleString(),
@@ -144,11 +152,22 @@ export default {
         // sort by total dose
         provArray.sort((a, b) => b.totalDose - a.totalDose)
         // show only 15 provinces
-        const slicedArr = provArray.slice(0, 15)
+        const slicedArr = provArray.slice(0, this.numProvinces)
         return slicedArr
       } else {
         return []
       }
+    },
+  },
+  methods: {
+    showMore() {
+      // when this function is clicked
+      // the computed function will automatically run
+      // updating the array
+      this.numProvinces = 77
+    },
+    showLess() {
+      this.numProvinces = 6
     },
   },
 }
@@ -176,14 +195,14 @@ export default {
   }
   thead,
   tbody {
-    background-color: #f0f0f0;
+    background-color: #f7f7f7;
   }
   tbody tr:not(:last-child) td {
     border-bottom: 1px solid #d1d5db;
   }
   td,
   th {
-    padding: 12px 16px;
+    padding: 16px 24px;
   }
   th {
     text-align: start;
