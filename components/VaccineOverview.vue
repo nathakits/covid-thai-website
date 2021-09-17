fdd0ac3ccae8af3f160ecae33c0f3827ba8246fc
Nathakit Sae-Tan <nathakit.tan@gmail.com>
2021-09-18 00:16:36 +0700

    add vaccination progress and goals

--- components/VaccineOverview.vue
+++ components/VaccineOverview.vue
@@ -1,38 +1,99 @@
 <template lang="pug">
-div(style="background-color:rgba(6,1,70,0.2);")
+div(style="background-color:rgba(6,1,70,0.15);")
   div.container.mx-auto.h-full
     div.vaccination-goal
-      div.grid.grid-cols-8.container-padding(
-        class="gap-4 sm:gap-4 md:gap-4"
-      )
-        div.rounded-md.bg-white.p-4.shadow-md.col-span-4(
-          v-for="(item, index) in mapArray"
-          class="md:col-span-2"
+      div.container-padding.grid.gap-4
+        div.grid.grid-cols-8(
+          class="gap-4 sm:gap-4 md:gap-4"
         )
-          div.w-full
-            span.font-medium.text-sm.text-gray-900(class="lg:text-base") {{ item.name }}
-          div.flex.flex-col.justify-between.pt-2
-            h2.text-xl.font-bold(
-              class="lg:text-3xl"
-              style="color:#090076;"
-            ) {{ item.total }}
-            div.w-full.border-b.my-2
-            div.flex.flex-wrap.justify-between.gap-2
-              div.flex.flex-col.justify-between(style="min-height:48px;")
-                div.text-sm.font-medium.text-gray-500 Doses
-                div.flex.items-center.gap-1
-                  plus-icon(style="opacity:0.8;")
-                  span.text-base.font-bold.text-gray-900 {{ item.daily }}
-              div.flex.flex-col.justify-between(style="min-height:48px;")
-                span.text-sm.font-medium.text-gray-500(
-                  v-if="item.percent"
-                ) Population
-                div.flex.items-center(
-                  :class="item.percent ? 'gap-1' : ''"
-                )
-                  template(v-if="item.percent")
-                    arrow-up(style="opacity:0.8;")
-                  div.font-bold {{ item.percent }}
+          div.rounded-md.bg-white.p-4.shadow-md.col-span-4(
+            v-for="(item, index) in mapArray"
+            class="md:col-span-2"
+          )
+            div.w-full
+              span.font-bold.text-sm.text-gray-900(class="lg:text-base") {{ item.name }}
+            div.flex.flex-col.justify-between.pt-2
+              h2.text-xl.font-bold(
+                class="lg:text-3xl"
+                style="color:#090076;"
+              ) {{ item.total }}
+              div.w-full.border-b.my-2
+              div.flex.flex-wrap.justify-between.gap-2
+                div.flex.flex-col.justify-between(style="min-height:48px;")
+                  div.text-sm.font-medium.text-gray-500 Doses
+                  div.flex.items-center.gap-1
+                    plus-icon(style="opacity:0.8;")
+                    span.text-base.font-bold.text-gray-900 {{ item.daily }}
+                div.flex.flex-col.justify-between(style="min-height:48px;")
+                  span.text-sm.font-medium.text-gray-500(
+                    v-if="item.percent"
+                  ) Population
+                  div.flex.items-center(
+                    :class="item.percent ? 'gap-1' : ''"
+                  )
+                    template(v-if="item.percent")
+                      arrow-up(style="opacity:0.8;")
+                    div.font-bold {{ item.percent }}
+        div.grid.grid-cols-8(
+          class="gap-4 sm:gap-4 md:gap-4"
+        )
+          div.rounded-md.bg-white.p-4.shadow-md.col-span-8(
+            class="md:col-span-4"
+          )
+            span.font-bold.text-sm.text-gray-900(class="lg:text-base") Vaccination Progress
+            div.progress.py-2
+              span.flex.h-4.overflow-hidden.rounded-lg.bg-gray-200.relative
+                span.bg-green-800.absolute.h-full(:style="`width:${firstDosePercentage}%`")
+                span.bg-green-600.absolute.h-full(:style="`width:${secondDosePercentage}%`")
+                span.bg-green-400.absolute.h-full(:style="`width:${thirdDosePercentage}%`")
+                //- span.bg-gray-200(class="w-4/12")
+            div.legend.flex.flex-wrap.gap-x-4.gap-y-1.pt-1
+              div.flex.items-center
+                span.dot.firstDose
+                span.text-sm.font-medium
+                  | 1st Dose - 
+                  strong {{ `${firstDosePercentage}%` }}
+              div.flex.items-center
+                span.dot.secondDose
+                span.text-sm.font-medium
+                  | 2nd Dose - 
+                  strong {{ `${secondDosePercentage}%` }}
+              div.flex.items-center
+                span.dot.thirdDose
+                span.text-sm.font-medium
+                  | 3rd Dose - 
+                  strong {{ `${thirdDosePercentage}%` }}
+          div.rounded-md.bg-white.p-4.shadow-md.col-span-8(
+            class="md:col-span-4"
+          )
+            div
+              div.flex.justify-between.items-center.flex-wrap.gap-2
+                div.flex.items-center.gap-2
+                  span.font-bold.text-sm.text-gray-900(class="lg:text-base")
+                    | 50M Goal
+                  div(title="1st Dose only")
+                    info-icon(
+                      :size="16"
+                      color="#6881d8"
+                    ) 
+                span.text-sm.font-bold {{ `${popGoal50}%` }}
+              div.progress.py-2
+                span.flex.h-4.overflow-hidden.rounded-lg.bg-gray-200
+                  span.bg-green-800(:style="`width:${popGoal50}%`")
+            div
+              div.flex.justify-between.items-center.flex-wrap.gap-2
+                div.flex.items-center.gap-2
+                  span.font-bold.text-sm.text-gray-900(class="lg:text-base")
+                    | 100M Goal
+                  div(title="Fully vaccinated")
+                    info-icon(
+                      :size="16"
+                      color="#6881d8"
+                    )
+                span.text-sm.font-bold {{ `${popGoal100}%` }}
+              div.progress.py-2
+                span.flex.h-4.overflow-hidden.rounded-lg.bg-gray-200
+                  span.bg-green-800(:style="`width:${popGoal100}%`")
 </template>
 
 <script>
@@ -40,9 +101,13 @@ import { mapGetters } from "vuex"
 
 export default {
   props: {
-    data: {
+    daily: {
       type: Object,
       default: () => {},
+    },
+    all: {
+      type: Array,
+      default: () => [],
     },
   },
   data() {
@@ -50,23 +115,23 @@ export default {
       array: [
         {
           name: "1st Dose Total",
-          total: this.data.people_vaccinated,
-          daily: this.data.first_dose_plus,
+          total: this.daily.people_vaccinated,
+          daily: this.daily.first_dose_plus,
         },
         {
           name: "2nd Dose Total",
-          total: this.data.people_fully_vaccinated,
-          daily: this.data.second_dose_plus,
+          total: this.daily.people_fully_vaccinated,
+          daily: this.daily.second_dose_plus,
         },
         {
           name: "3rd Dose Total",
-          total: this.data.booster_vaccinated,
-          daily: this.data.third_dose_plus,
+          total: this.daily.booster_vaccinated,
+          daily: this.daily.third_dose_plus,
         },
         {
           name: "Total Dose",
-          total: this.data.total_vaccinations,
-          daily: this.data.total_dose_plus,
+          total: this.daily.total_vaccinations,
+          daily: this.daily.total_dose_plus,
         },
       ],
     }
@@ -74,12 +139,63 @@ export default {
   computed: {
     ...mapGetters({
       population: "thPopulation",
+      popGoal1: "popGoal1",
+      popGoal2: "popGoal2",
     }),
+    popGoal50() {
+      if (this.all) {
+        const latest = this.all[this.all.length - 1]
+        const goal = ((latest.people_vaccinated / this.popGoal1) * 100).toFixed(
+          2
+        )
+        return goal
+      } else {
+        return 0
+      }
+    },
+    popGoal100() {
+      if (this.all) {
+        const latest = this.all[this.all.length - 1]
+        const goal = (
+          (latest.people_fully_vaccinated / this.popGoal2) *
+          100
+        ).toFixed(2)
+        return goal
+      } else {
+        return 0
+      }
+    },
+    firstDosePercentage() {
+      if (this.all) {
+        const latest = this.all[this.all.length - 1]
+        return ((latest.people_vaccinated / this.population) * 100).toFixed(2)
+      } else {
+        return 0
+      }
+    },
+    secondDosePercentage() {
+      if (this.all) {
+        const latest = this.all[this.all.length - 1]
+        const vaccinated = latest.people_fully_vaccinated
+        return ((vaccinated / this.population) * 100).toFixed(2)
+      } else {
+        return 0
+      }
+    },
+    thirdDosePercentage() {
+      if (this.all) {
+        const latest = this.all[this.all.length - 1]
+        const vaccinated = latest.booster_vaccinated
+        return ((vaccinated / this.population) * 100).toFixed(2)
+      } else {
+        return 0
+      }
+    },
     mapArray() {
-      if (this.data) {
-        const dailyfirst = this.data.first_dose_plus.replace(/,/g, "")
-        const dailysecond = this.data.second_dose_plus.replace(/,/g, "")
-        const dailythird = this.data.third_dose_plus.replace(/,/g, "")
+      if (this.daily) {
+        const dailyfirst = this.daily.first_dose_plus.replace(/,/g, "")
+        const dailysecond = this.daily.second_dose_plus.replace(/,/g, "")
+        const dailythird = this.daily.third_dose_plus.replace(/,/g, "")
         const firstPercentage = (dailyfirst / this.population) * 100
         const secondPercentage = (dailysecond / this.population) * 100
         const thirdPercentage = (dailythird / this.population) * 100
@@ -136,3 +252,27 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+// legend
+.dot {
+  display: inline-block;
+  border-radius: 50%;
+  width: 12px;
+  height: 12px;
+  margin-right: 8px;
+
+  &.firstDose {
+    background-color: rgb(168, 218, 181);
+  }
+  &.secondDose {
+    background-color: rgb(91, 185, 116);
+  }
+  &.thirdDose {
+    background-color: rgb(12, 132, 63);
+  }
+  &.unvaccinated {
+    background-color: rgb(12, 132, 63);
+  }
+}
+</style>
